2b278aaed274256813eb395fa810b6720bf34d2c
Nahuel Verdecchia <nahvergon@gmail.com>
2023-12-07 01:09:22 +0100

    Day 5 Optimized (90ms!)

--- Day5_2_Seeds/index.js
+++ Day5_2_Seeds/index.js
@@ -9,42 +9,88 @@ const lines = fs.readFileSync(path.resolve(__dirname, INPUT_FILE), 'utf-8').spli
 
 const seedRanges = lines[0].split(' ').slice(1).map(s => +s);
 
-function travelSeed(maps, seed) {
-    let rollingValue = seed;
-    let intermediateSteps = [seed];
-    for (let i = 0; i < maps.length; i++) {
-        const mapStep = maps[i];
-
-        const toApply = mapStep.find(m => rollingValue >= m.origin && rollingValue <= m.origin + m.step);
-        // if (!toApply) rollingValue = rollingValue;
-        if (toApply) rollingValue = toApply.destination + rollingValue - toApply.origin;
-        intermediateSteps.push(rollingValue);
-    }
-    return {
-        value: rollingValue,
-        steps: intermediateSteps,
-    };
-}
-
 function travelSeedRange(maps, seedStart, seedEnd) {
-    const ranges = [[seedStart, seedEnd]];
-    const nextRanges = [];
+    let ranges = [[seedStart, seedEnd]];
+    let stepRanges = [];
 
     for (let i = 0; i < maps.length; i++) {
         const mapStep = maps[i];
 
-        // Calculate intersections with ranges.
+        const nextRanges = [];
+
+        // Calculate all possible ranges via intersections with each map
         for (let r = 0; r < ranges.length; r++) {
             const [ start, end ] = ranges[r];
-            for (let m = 0; m < mapStep.length; m++) {
-                if (m.origin > start && (m.origin + m.step))
+
+            // First eliminate maps that are outside of the range
+            const validMaps = mapStep.filter(m => m.end >= start && m.start <= end);
+
+            /**
+             * Since maps are ordered by start. We will always encounter:
+             * [                 original range                            ]
+             *[ range starts early]
+             *                       [ range within]               [range ends late]
+             * [range mapped 1    ][][range mapped3][rangenormal 4][rmappd5]
+             * 
+             * To calculate we will take a pivot starting at the range and search for a map
+             * that starts before or at the pivot and ends after or at the pivot.
+             * IF found we create a out-range from the pivot to the end of the map
+             * IF not found, we find the next map
+             *    IF found, we create an out-range from the pivot to the start of the map and move the pivot
+             *    IF not found, we create an out-range from the pivot to the end of the range and end
+             */
+
+            // We make sure to map to destination when needed
+            let pivot = start;
+            while (pivot <= end) {
+                const nextMapAtStart = validMaps.find(m => m.start <= pivot && m.end >= pivot);
+                if (nextMapAtStart) {
+                    if (end > nextMapAtStart.end) {
+                        // More to go
+                        nextRanges.push([nextMapAtStart.destination + (pivot - nextMapAtStart.start), nextMapAtStart.destination + nextMapAtStart.end - nextMapAtStart.start]);
+                        pivot = nextMapAtStart.end + 1;
+                        continue;
+                    } else {
+                        // We done
+                        nextRanges.push([nextMapAtStart.destination + (pivot - nextMapAtStart.start), nextMapAtStart.destination + (end - nextMapAtStart.start)]);
+                        break;
+                    }
+                }
+
+                // No map found. Search for the next
+                const nextSpacedMap = validMaps.find(m => m.start >= pivot);
+                if (nextSpacedMap) {
+                    // Map found. Create to ranges. One from pivot to mapstart. Another from  mapstart to whichever end
+                    nextRanges.push([pivot, nextSpacedMap.start - 1]);
+
+                    // Since this ranges start at the MAP, no need to do math on range start
+                    if (end > nextSpacedMap.end) {
+                        // More to go
+                        nextRanges.push([nextSpacedMap.destination, nextSpacedMap.destination + nextSpacedMap.end - nextSpacedMap.start]);
+                        pivot = nextSpacedMap.end + 1;
+                        continue;
+                    } else {
+                        // We done
+                        nextRanges.push([nextSpacedMap.destination, nextSpacedMap.destination + (end - nextSpacedMap.start)]);
+                        break;
+                    }
+                }
+
+                // No more ranges to go. Create a range just for this
+                nextRanges.push([pivot, end]);
+                break;
             }
         }
+
+        stepRanges.push(nextRanges);
+        ranges = nextRanges; // swap ranges for next map step
     }
+
+    return { ranges, stepRanges};
 }
 
 // each element is a step
-// each step is an array of {origin,step,destination}
+// each step is an array of {start,end,destination}
 const maps = [];
 
 const mapLines = lines.flatMap((l, i) => l.includes('map:') ? i : []);
@@ -54,26 +100,25 @@ for (const mapLineIndex of mapLines) {
 
     let lineIdx = mapLineIndex + 1;
     while (lineIdx < lines.length && lines[lineIdx].length > 0) {
-        const [destination, origin, step] = lines[lineIdx].split(' ', 3).map(t => +t);
-        currentMap.push({ destination, origin, step, });
+        const [destination, start, step] = lines[lineIdx].split(' ', 3).map(t => +t);
+        currentMap.push({ destination, start, end: start + step - 1, });
 
         lineIdx++;
     }
 
+    currentMap.sort((a, b) => a.start - b.start);
+
     maps.push(currentMap);
 }
 
-const traveller = travelSeed.bind(null, maps);
+const traveller = travelSeedRange.bind(null, maps);
 
 let lowestSeed = Number.MAX_SAFE_INTEGER;
 
 for (let i = 0; i < seedRanges.length; i += 2) {
-    for (let seed = seedRanges[i]; seed < seedRanges[i] + seedRanges[i + 1]; seed++) {
-        const { value, steps } = traveller(seed);
-        if (value < lowestSeed) lowestSeed = value;
-        //console.log(`Seed ${String(seed).padStart(10, ' ')} => ${steps.map(s => String(s).padStart(10, ' ')).join(' => ')}`);
-    }
-    console.log('Pair done');
+    const travel = traveller(seedRanges[i], seedRanges[i] + seedRanges[i + 1] - 1);
+    const localMin = travel.ranges.sort((a, b) => a[0] - b[0]).at(0)?.at(0) ?? Number.MAX_SAFE_INTEGER;
+    if (localMin < lowestSeed) lowestSeed = localMin;
 }
 
 
